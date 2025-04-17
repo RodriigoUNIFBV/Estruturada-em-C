ff36bf872bc46b26c019af6f56ecf56eac445c31
RodriigoUNIFBV <202408167393@alunos.unifbv.edu.br>
2025-04-17 20:33:13 -0300

    Create loopFOR.c

---
+++ loopFOR.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, numi;
+    for (num = 1; num < 11; num++){
+        printf("%d\n", num);
+    }
+    for (num = 10; num > 0; num--){
+        printf("%d\n", num);
+    }
+    return 0;
+}
