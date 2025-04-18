1e6bce953c23998687faf421d86ce47085b94261
RodriigoUNIFBV <202408167393@alunos.unifbv.edu.br>
2025-04-17 21:07:37 -0300

    Create WHILEatePositivo.c
    ate o numero ser positivo !

---
+++ WHILEatePositivo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{   
+    int numero = 0;
+    while(numero <= 0){
+        printf("Digite um número inteiro: ");
+        scanf("%d", &numero);
+    }
+    printf("Obrigado!");
+  
+    return 0;
+}
